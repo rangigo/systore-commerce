fd3ae9b644f09a85394e36f80662524c3320c835
Phuoc <phuoc@hey.com>
2020-11-14 04:35:06 +0200

    🚧 Test logo change

--- components/common/Avatar/Avatar.tsx
+++ components/common/Avatar/Avatar.tsx
@@ -1,4 +1,4 @@
-import cn from 'classnames'
+import Image from 'next/image'
 import { FC, useState, useMemo, useRef, useEffect } from 'react'
 import { getRandomPairOfColors } from '@lib/colors'
 
@@ -22,6 +22,15 @@ const Avatar: FC<Props> = ({}) => {
       ref={ref}
       className="inline-block h-8 w-8 rounded-full border-2 border-primary hover:border-secondary focus:border-secondary transition linear-out duration-150"
     >
+      <Image
+        className=''
+        src="/systore-icon.jpg"
+        alt="Logo Image"
+        // The cart item image is already optimized and very small in size
+        unoptimized
+        width='32px'
+        height='32px'
+      />
       {/* Add an image - We're generating a gradient as placeholder  <img></img> */}
     </div>
   )
--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -28,10 +28,10 @@ const Footer: FC<Props> = ({ className, pages }) => {
           <div className="col-span-1 lg:col-span-2">
             <Link href="/">
               <a className="flex flex-initial items-center font-bold md:mr-24">
-                <span className="rounded-full border border-gray-700 mr-2">
+                <span className="rounded-full">
                   <Logo />
                 </span>
-                <span>ACME</span>
+                <span>SY Store</span>
               </a>
             </Link>
           </div>
